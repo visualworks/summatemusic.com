3ebc1159e237ab073a8e460a046143095522a4bd
Henrique Mattos <henrique@visualworks.com.br>
2019-11-03 13:18:20 +0100

    [VW] Adjust streaming time and countdown to respect day interval
    After today's streaming at 16h GMT-3, next is on Tuesday 05/11/2019 and
    we need to make sure the video player won't be loaded tomorrow at 16h.
    
    Milestone: kick-off
    Project: Website
    Signed-off-by: Henrique Mattos <henrique.mattos@sap.com>

--- js/summatemusic.js
+++ js/summatemusic.js
@@ -6,16 +6,21 @@ window.addEventListener("load", (event) => {
 
     const now = new Date();
     const isStreamingTime = (now.getUTCDate() === 3 || now.getUTCDate() === 5) && now.getUTCHours() >= 19 && now.getUTCHours() <= 24;
+    const isDayInterval = (now.getUTCDate() > 3 && now.getUTCDate() < 5) || (now.getUTCDate() === 3 && now.getUTCHours() >= 21);
     if (isStreamingTime) {
         videoPlayerElement.className = "";
     } else {
         videoPlayerElement.className = "is-hidden";
-        setInterval(() => {
-            const now = new Date();
-            const startUTCTime = Date.UTC("2019", "11", "3", "19", "00", "00");
-            const nowUTCTime = Date.UTC(now.getUTCFullYear(), now.getUTCMonth() + 1, now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
-            const countdown = new Date(startUTCTime - nowUTCTime);
-            domElement.innerText = `${countdown.getUTCHours()} horas ${countdown.getUTCMinutes()} minutos ${countdown.getUTCSeconds()} segundos`;
-        }, 1000);
+        if (isDayInterval) {
+            domElement.innerText = "05/11/2019 16:00h";
+        } else {
+            setInterval(() => {
+                const now = new Date();
+                const startUTCTime = Date.UTC("2019", "11", "3", "19", "00", "00");
+                const nowUTCTime = Date.UTC(now.getUTCFullYear(), now.getUTCMonth() + 1, now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
+                const countdown = new Date(startUTCTime - nowUTCTime);
+                domElement.innerText = `${countdown.getUTCHours()} horas ${countdown.getUTCMinutes()} minutos ${countdown.getUTCSeconds()} segundos`;
+            }, 1000);
+        }
     }
 });
