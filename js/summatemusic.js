d65e4783293aa908e8b050d401d8a8823986bf62
Henrique Mattos <henrique@visualworks.com.br>
2019-11-16 21:35:50 +0100

    [VW] Adjust streaming for the weekend
    TBD
    
    Milestone: Data Management
    Project: Website
    Signed-off-by: Henrique Mattos <henrique.mattos@sap.com>

--- js/summatemusic.js
+++ js/summatemusic.js
@@ -5,8 +5,8 @@ window.addEventListener("load", () => {
     videoPlayerElement.height = screen.height;
 
     const now = new Date();
-    const isStreamingTime = (now.getUTCDate() === 10 || now.getUTCDate() === 12) && now.getUTCHours() >= 20 && now.getUTCHours() <= 23;
-    const isDayInterval = (now.getUTCDate() > 8 && now.getUTCDate() < 10) || (now.getUTCDate() === 10 && now.getUTCHours() >= 23);
+    const isStreamingTime = (now.getUTCDate() === 16 || now.getUTCDate() === 17) && now.getUTCHours() >= 20 && now.getUTCHours() <= 23;
+    const isDayInterval = (now.getUTCDate() > 17 && now.getUTCDate() < 19) || (now.getUTCDate() === 17 && now.getUTCHours() >= 23);
     if (isStreamingTime) {
         videoPlayerElement.className = "";
     } else {
@@ -16,7 +16,7 @@ window.addEventListener("load", () => {
         } else {
             setInterval(() => {
                 const now = new Date();
-                const startUTCTime = Date.UTC("2019", "11", "10", "20", "00", "00");
+                const startUTCTime = Date.UTC("2019", "11", "16", "20", "00", "00");
                 const nowUTCTime = Date.UTC(now.getUTCFullYear(), now.getUTCMonth() + 1, now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
                 const countdown = new Date(startUTCTime - nowUTCTime);
                 domElement.innerText = `${countdown.getUTCHours()} horas ${countdown.getUTCMinutes()} minutos ${countdown.getUTCSeconds()} segundos`;
