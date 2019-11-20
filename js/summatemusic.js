903f0756185c3b67ca05aab57297bc2d8f82f3d2
Henrique Mattos <henrique@visualworks.com.br>
2019-11-20 21:19:23 +0100

    [VW] Open stremaing for 2019-11-20
    TBD
    
    Milestone: Data Management
    Project: Website
    Signed-off-by: Henrique Mattos <henrique.mattos@sap.com>

--- js/summatemusic.js
+++ js/summatemusic.js
@@ -5,18 +5,18 @@ window.addEventListener("load", () => {
     videoPlayerElement.height = screen.height;
 
     const now = new Date();
-    const isStreamingTime = (now.getUTCDate() === 16 || now.getUTCDate() === 17) && now.getUTCHours() >= 20 && now.getUTCHours() <= 23;
-    const isDayInterval = (now.getUTCDate() > 17 && now.getUTCDate() < 19) || (now.getUTCDate() === 17 && now.getUTCHours() >= 23);
+    const isStreamingTime = (now.getUTCDate() === 20 || now.getUTCDate() === 22) && now.getUTCHours() >= 20 && now.getUTCHours() <= 23;
+    const isDayInterval = (now.getUTCDate() > 20 && now.getUTCDate() < 22) || (now.getUTCDate() === 17 && now.getUTCHours() >= 23);
     if (isStreamingTime) {
         videoPlayerElement.className = "";
     } else {
         videoPlayerElement.className = "is-hidden";
         if (isDayInterval) {
-            domElement.innerText = "10/11/2019 17:00h";
+            domElement.innerText = "22/11/2019 17:00h";
         } else {
             setInterval(() => {
                 const now = new Date();
-                const startUTCTime = Date.UTC("2019", "11", "16", "20", "00", "00");
+                const startUTCTime = Date.UTC("2019", "11", "20", "20", "00", "00");
                 const nowUTCTime = Date.UTC(now.getUTCFullYear(), now.getUTCMonth() + 1, now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
                 const countdown = new Date(startUTCTime - nowUTCTime);
                 domElement.innerText = `${countdown.getUTCHours()} horas ${countdown.getUTCMinutes()} minutos ${countdown.getUTCSeconds()} segundos`;
