1c61050c1c3e3c61eec9da6029df8a0236f50bf6
Henrique Mattos <henrique@visualworks.com.br>
2019-11-27 22:37:10 +0100

    [VW] Setup countdown to read data from flat file
    Make the countdown start and stop automatically using intervals and
    add list of streaming to be loaded dynamically from JSON file.
    
    Milestone: Data Management
    Project: Website
    Signed-off-by: Henrique Mattos <henrique.mattos@sap.com>

--- js/summatemusic.js
+++ js/summatemusic.js
@@ -3,24 +3,71 @@ window.addEventListener("load", () => {
     const videoPlayerElement = document.getElementById("live-streaming-video-player");
     videoPlayerElement.width = screen.width;
     videoPlayerElement.height = screen.height;
-
-    const now = new Date();
-    const isStreamingTime = (now.getUTCDate() === 20 || now.getUTCDate() === 22) && now.getUTCHours() >= 20 && now.getUTCHours() <= 23;
-    const isDayInterval = (now.getUTCDate() > 20 && now.getUTCDate() < 22) || (now.getUTCDate() === 17 && now.getUTCHours() >= 23);
-    if (isStreamingTime) {
-        videoPlayerElement.className = "";
-    } else {
-        videoPlayerElement.className = "is-hidden";
-        if (isDayInterval) {
-            domElement.innerText = "22/11/2019 17:00h";
-        } else {
-            setInterval(() => {
-                const now = new Date();
-                const startUTCTime = Date.UTC("2019", "11", "20", "20", "00", "00");
-                const nowUTCTime = Date.UTC(now.getUTCFullYear(), now.getUTCMonth() + 1, now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
-                const countdown = new Date(startUTCTime - nowUTCTime);
-                domElement.innerText = `${countdown.getUTCHours()} horas ${countdown.getUTCMinutes()} minutos ${countdown.getUTCSeconds()} segundos`;
-            }, 1000);
+    // let pastEvents = [];
+    let futureEvents = [];
+    let currentEvent = [];
+    fetch("/js/data.json", {
+        method: "GET",
+        mode: "same-origin",
+        cache: "default"
+    }).then((response) => {
+        if (!response.ok) {
+            console.error(`Response is not ok: ${JSON.stringify(response)}`);
         }
-    }
+        return response.json();
+    }).then((json) => {
+        if (json && json.events) {
+            let now = new Date();
+            json.events.forEach((event) => {
+                const eventDate = new Date(event.date);
+                if (eventDate.getUTCMonth() < now.getUTCMonth() || (eventDate.getUTCMonth() === now.getUTCMonth() && eventDate.getUTCDate() < now.getUTCDate())) {
+                    pastEvents.push(event);
+                } else if (eventDate.getUTCMonth() > now.getUTCMonth() || (eventDate.getUTCMonth() === now.getUTCMonth() && eventDate.getUTCDate() > now.getUTCDate())) {
+                    futureEvents.push(event);
+                } else if(eventDate.getUTCMonth() === now.getUTCMonth() && eventDate.getUTCDate() === now.getUTCDate()) {
+                    currentEvent.push(event);
+                }
+            });
+            if (currentEvent.length === 0 && futureEvents.length > 0) {
+                const eventDate = futureEvents[0].date.split("-");
+                domElement.innerText = `${eventDate[2]}/${eventDate[1]} ${futureEvents[0].start}`;
+            } else if (currentEvent.length > 0) {
+                const eventStart = new Date(`${currentEvent[0].date}T${currentEvent[0].start}:00.000-03:00`);
+                const eventEnd = new Date(`${currentEvent[0].date}T${currentEvent[0].end}:00.000-03:00`);
+                let nowUTCTime = Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
+                const startUTCTime = Date.UTC(eventStart.getUTCFullYear(), eventStart.getUTCMonth(), eventStart.getUTCDate(), eventStart.getUTCHours(), eventStart.getUTCMinutes(), "00");
+                const endUTCTime = Date.UTC(eventEnd.getUTCFullYear(), eventEnd.getUTCMonth(), eventEnd.getUTCDate(), eventEnd.getUTCHours(), eventEnd.getUTCMinutes(), "00");
+                const isEventWindow = Boolean(nowUTCTime > startUTCTime && nowUTCTime < endUTCTime);
+                const isBeforeEvent = Boolean(nowUTCTime < startUTCTime);
+                const isPastEvent = Boolean(nowUTCTime > endUTCTime);
+                if (isEventWindow) {
+                    videoPlayerElement.className = "";
+                    setInterval(() => {
+                        now = new Date();
+                        nowUTCTime = Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
+                        if (new Date(nowUTCTime) > new Date(eventEnd)) {
+                            location.reload();
+                        }
+                    }, 5000);
+                } else if(isBeforeEvent) {
+                    videoPlayerElement.className = "is-hidden";
+                    setInterval(() => {
+                        now = new Date();
+                        nowUTCTime = Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
+                        const countdown = new Date(startUTCTime - nowUTCTime);
+                        if (new Date(startUTCTime) < new Date(nowUTCTime)) {
+                            location.reload();
+                        }
+                        domElement.innerText = `${countdown.getUTCHours()} horas ${countdown.getUTCMinutes()} minutos ${countdown.getUTCSeconds()} segundos`;
+                    }, 1000);
+                } else if (isPastEvent) {
+                    videoPlayerElement.className = "is-hidden";
+                    if (futureEvents.length > 0) {
+                        const eventDate = futureEvents[0].date.split("-");
+                        domElement.innerText = `${eventDate[2]}/${eventDate[1]} ${futureEvents[0].start}`;
+                    }
+                }
+            }
+        }
+    });
 });
